ff71f0ad10c0443cd210547a5c12b598f489a53e
Sri Manikanta <50536815+talatam143@users.noreply.github.com>
2021-05-24 10:09:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = requires("express");
-const bodyParser = requires("body-parser");
-const mongoose = requires("mongoose");
-const cors = requires("cors");
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 import postRoutes from "./routes/posts.js"
 import dotenv from "dotenv"
 
